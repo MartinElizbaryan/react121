ce14e4094a7be16bc885ef2e983a32b509ba3f3d
Martin Elizbaryan <martin.elizbaryan@gmail.com>
2023-06-06 22:42:43 +0400

    Lesson-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { greaterOrEqualZero } from "./helpers.js";
+// import * as x from "./helpers.js";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(7);
+  const [title, setTitle] = useState("Count");
+  const arr = [1, 2, 3];
+
+  const handleClick = (value) => {
+    const result = count + value;
+
+    // if (greaterOrEqualZero(result)) {
+    //   setCount(result);
+    // } else {
+    //   setCount(0);
+    // }
+
+    setCount(greaterOrEqualZero(result) ? result : 0);
+  };
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ marginRight: 4 }} onClick={() => handleClick(1)}>
+        +1
+      </button>
+
+      <button style={{ marginRight: 4 }} onClick={() => handleClick(5)}>
+        +5
+      </button>
+
+      <button style={{ marginRight: 4 }} onClick={() => handleClick(10)}>
+        +10
+      </button>
+
+      <button style={{ marginRight: 4 }} onClick={() => handleClick(-5)}>
+        -5
+      </button>
+
+      <input style={{ marginRight: 4 }} value={title} onChange={handleChange} />
+
+      <br />
+      <span>
+        {title}: {count}
+        <br />
+        {arr.map((val) => {
+          return (
+            <div>
+              <h1>{val}</h1>
+              <h2>{val}</h2>
+            </div>
+          );
+        })}
+      </span>
     </div>
   );
-}
+};
 
 export default App;
+
+// = useState(100)
+// [100, fn]
---
+++ src/helpers.js
@@ -0,0 +1,1 @@
+export const greaterOrEqualZero = (value) => value >= 0;
